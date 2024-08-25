8a83f1589c5cadfff5d11cbbe958381a9fc0ee71
Daniel Kert <danielkertdev@gmail.com>
2024-08-25 19:38:30 +0300

    Fixade konjugatregeln
    Fixade version 1 konjukatsregeln

--- include/navigation/screen/options/algebra/konjugatregeln.c
+++ include/navigation/screen/options/algebra/konjugatregeln.c
@@ -1,7 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+#include "../../../../utils/dash_box.h"
+
+
 int konjugatregeln()
 {
-    int x;
-    printf("Skriv en sifra \n");
-    scanf("%d", &x);
-    printf("Din sifra + 5 blir %d \n", x + 5);
+
+    // Variables
+    int a, b, svar;
+
+    char *text[] = {
+        "Konjugatregeln",
+        "Beskrivning:",
+        "Konjugatregeln anvands for att multiplicera tva parentesuttryck",
+        "bestaende av tva termer var dar ena uttrycket beskriver summan av",
+        "termerna och det andra uttrycket beskriver differensen mellan termerna.",
+        "Formel:",
+        "(a + b)(a - b)=a^2 - b^2",
+    };
+
+    dash_box(text, 7, 100);
+
+    printf("\n Skriv in termen a:\n");
+    scanf(" %d", &a);
+
+    printf("\n Skriv in termen b:\n");
+    scanf(" %d", &b);
+
+    svar = (a + b) * (a - b);
+    printf("a^2 - b^2 = %d\n", svar);
+
 }
